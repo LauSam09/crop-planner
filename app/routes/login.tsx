9a4596fc852924dc3b6acbe5b3ce50667e76d029
Laurence Ede <laurence.ede@gmail.com>
2023-05-06 22:07:47 +0100

    Add dark mode styling for login form

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -61,61 +61,65 @@ export default function Login() {
   const actionData = useActionData();
 
   return (
-    <div className="mx-auto max-w-md px-4 py-24 sm:px-6 sm:py-32 lg:px-8">
-      <h1 className="text-base font-semibold leading-7 text-gray-900">Login</h1>
+    <div className="dark:bg-gray-900 h-screen flex justify-center items-center">
+      <div className="p-4 w-80 dark:bg-gray-800 rounded">
+        <h1 className="text-base font-semibold leading-7 text-gray-900 dark:text-white">
+          Login
+        </h1>
 
-      <Form method="post">
-        {actionData?.errors.form ? (
-          <p className="mt-1 text-sm text-red-600 dark:text-red-500">
-            {actionData.errors.form}
+        <Form method="post">
+          {actionData?.errors.form ? (
+            <p className="mt-1 text-sm text-red-600 dark:text-red-500">
+              {actionData.errors.form}
+            </p>
+          ) : null}
+          <p>
+            <label className="block text-sm font-medium leading-6 text-gray-900 dark:text-white">
+              Email address
+              <input
+                type="email"
+                name="email"
+                defaultValue={actionData?.values.email}
+                className="block w-full rounded-md border py-1.5 px-2 mt-1 text-gray-900 shadow-sm placeholder:text-gray-400 outline-none focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+              />
+            </label>
           </p>
-        ) : null}
-        <p>
-          <label className="block text-sm font-medium leading-6 text-gray-900">
-            Email address
-            <input
-              type="email"
-              name="email"
-              defaultValue={actionData?.values.email}
-              className="block w-full rounded-md border py-1.5 px-2 mt-1 text-gray-900 shadow-sm placeholder:text-gray-400 outline-none focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
-            />
-          </label>
-        </p>
-        {actionData?.errors.email ? (
-          <p className="mt-1 text-sm text-red-600 dark:text-red-500">
-            {actionData.errors.email}
+          {actionData?.errors.email ? (
+            <p className="mt-1 text-sm text-red-600 dark:text-red-500">
+              {actionData.errors.email}
+            </p>
+          ) : null}
+          <p>
+            <label className="block text-sm font-medium leading-6 text-gray-900 dark:text-white mt-2">
+              Password
+              <input
+                type="password"
+                name="password"
+                className="block w-full rounded-md border py-1.5 px-2 mt-1 text-gray-900 shadow-sm placeholder:text-gray-400 outline-none focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+              />
+            </label>
           </p>
-        ) : null}
-        <p>
-          <label className="block text-sm font-medium leading-6 text-gray-900 mt-2">
-            Password
-            <input
-              type="password"
-              name="password"
-              className="block w-full rounded-md border py-1.5 px-2 mt-1 text-gray-900 shadow-sm placeholder:text-gray-400 outline-none focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
-            />
-          </label>
-        </p>
-        {actionData?.errors.password ? (
-          <p className="mt-1 text-sm text-red-600 dark:text-red-500">
-            {actionData.errors.password}
-          </p>
-        ) : null}
-        <div className="flex justify-end items-center gap-2 w-full mt-2">
-          <Link
-            to="/signup"
-            className="rounded-md px-3 py-[6px] font-medium text-green-600 dark:text-green-500 hover:underline focus-visible:outline focus-visible:outline-2"
-          >
-            Create Account
-          </Link>
-          <button
-            type="submit"
-            className="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
-          >
-            Login
-          </button>
-        </div>
-      </Form>
+          {actionData?.errors.password ? (
+            <p className="mt-1 text-sm text-red-600 dark:text-red-500">
+              {actionData.errors.password}
+            </p>
+          ) : null}
+          <div className="flex justify-end items-center gap-2 w-full mt-2">
+            <Link
+              to="/signup"
+              className="rounded-md px-3 py-[6px] font-medium text-green-600 dark:text-green-500 hover:underline focus-visible:outline focus-visible:outline-2"
+            >
+              Create Account
+            </Link>
+            <button
+              type="submit"
+              className="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+            >
+              Login
+            </button>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 }
