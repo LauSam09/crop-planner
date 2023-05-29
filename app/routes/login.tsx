c51e07b1955d11f2597de2d3d8626af7947cb6c4
Laurence Ede <laurence.ede@gmail.com>
2023-05-29 17:13:00 +0100

    Use autocomplete attributes on auth forms

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -101,6 +101,7 @@ export default function Login() {
               <input
                 type="password"
                 name="password"
+                autoComplete="current-password"
                 className="mt-1 block w-full rounded-md border px-2 py-1.5 text-gray-900 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
               />
             </label>
--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -96,6 +96,7 @@ export default function SignUp() {
               <input
                 type="password"
                 name="password"
+                autoComplete="new-password"
                 className="mt-1 block w-full rounded-md border px-2 py-1.5 text-gray-900 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
               />
             </label>
