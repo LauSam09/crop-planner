a984b615a5b4173bcba26410b48f1965dd986a9d
Laurence Ede <laurence.ede@gmail.com>
2023-08-19 15:32:22 +0100

    Adjust sowing date wording

--- app/components/SowingDetails.tsx
+++ app/components/SowingDetails.tsx
@@ -1,4 +1,4 @@
-import { format, isFuture, differenceInWeeks } from "date-fns";
+import { format, isFuture, differenceInWeeks, isThisWeek } from "date-fns";
 import type { Stage } from "~/models/crop";
 
 interface SowingDetailProps {
@@ -11,10 +11,32 @@ const SowingDetails = ({ number, currentStage, date }: SowingDetailProps) => {
   const today = new Date();
   let text: string;
 
+  const getFutureWording = () => {
+    const weekCount = differenceInWeeks(date, today, {
+      roundingMethod: "ceil",
+    });
+
+    return isThisWeek(date, { weekStartsOn: 1 })
+      ? "this week"
+      : weekCount === 1
+      ? `next week`
+      : `in ${weekCount} weeks`;
+  };
+
+  const getPastWording = () => {
+    const weekCount = differenceInWeeks(today, date, {
+      roundingMethod: "ceil",
+    });
+
+    return isThisWeek(date, { weekStartsOn: 1 })
+      ? "this week"
+      : weekCount === 1
+      ? `last week`
+      : `${weekCount} weeks ago`;
+  };
+
   const formattedDate = format(date, "dd/MM/yy");
-  const interval = isFuture(date)
-    ? `in ${differenceInWeeks(date, today)} weeks`
-    : `${differenceInWeeks(today, date)} weeks ago`;
+  const interval = isFuture(date) ? getFutureWording() : getPastWording();
 
   switch (currentStage) {
     case "growing":
